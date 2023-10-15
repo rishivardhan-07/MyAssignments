fe801d9211eeda36a689f94e68a1d5252ff3e555
rishivardhan-07 <147966507+rishivardhan-07@users.noreply.github.com>
2023-10-15 13:43:40 +0530

    Add files via upload
    week1 day 1

---
+++ day1/DataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int dateOfBirth= 1999;
+		float percentangeValue= 98.2f;
+		char alphabetSample= 'h';
+		long mobileNumber= 8668170325l;
+		boolean accuracyLevel= false;
+		String namePerson= "Rishi";  
+		double checkVal= 2.874;
+
+		System.out.println(dateOfBirth);
+		System.out.println(percentangeValue);
+		System.out.println(alphabetSample);
+		System.out.println(mobileNumber);
+		System.out.println(accuracyLevel);
+		System.out.println(namePerson);
+		System.out.println(checkVal);
+
+
+	}
+
+}
---
+++ day1/IfELseCondition.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class IfELseCondition {
+	public static void main(String[] args) {
+		int number1= 45;
+		if (number1>0) {
+			System.out.println("The number is Positive");
+		}
+		else {
+			System.out.println("The number is Negative");
+
+		}
+
+
+	}
+
+}
